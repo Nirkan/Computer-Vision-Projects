cbe452af8ae0e5c261d88321b1d538445599fcaf
Niraj Kandpal <39731253+Nirkan@users.noreply.github.com>
2020-03-04 22:22:58 +0100

    Basics- opening and visualizing

---
+++ OpenCV-Tutorials/opcv1.py
@@ -0,0 +1,23 @@
+# Importing packages
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Reading images
+img = cv2.imread('kingfisher.jpg', 0)
+
+# Print shape of image
+print(img.shape)
+
+# Plotting image using cv2
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Plotting in matplotlib
+'''
+plt.imshow(img, cmap= 'gray', interpolation = 'bicubic')
+plt.plot([50, 100], [400, 100], 'c', linewidth = 10)
+plt.show()
+'''
+
